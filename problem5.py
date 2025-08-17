c9a5b1bfd3616ecfcf0feb232f7591f94cb9ce02
priyonti22 <priyonti768@gmail.com>
2025-08-17 19:25:26 +0600

    problem5

---
+++ problem5.py
@@ -0,0 +1,13 @@
+marks=int(input("Enter marks: "))
+if(marks>=90 and marks<=100):
+    print("Grade A")
+elif(marks>=80 and marks<89):
+    print("Grade B")
+elif(marks>=70 and marks<79):
+    print("Grade C")
+elif(marks>=60 and marks<69):
+    print("Grade D")
+else:
+    print("Grade F")
+
+    
